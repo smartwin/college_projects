bcfd187927c1c6dd1447f8f58d2b678d6e10ce40
Myia Dickens <myia.dickens@gmail.com>
2015-08-14 07:25:45 -0700

    Update bank.cpp

--- CIS_C/bank.cpp
+++ CIS_C/bank.cpp
@@ -1,8 +1,10 @@
-//Assignment 9
-//This program will assist the user in creating
-//customer bank accounts. The user will be able to
-//edit this bank accounts, as well as print them out 
-
+// Myia Dickens
+// 1245624
+// Assignment 09
+// The program will simulate a customer account manager
+// The user will be able to input a customer's account information
+// using structures. The user will also be able to edit the information inputed,
+// as well as print the customer accounts. 
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -34,7 +36,7 @@ int bankMenu()
    cin >> option;
     
    //Input Validation
-   if(option <= 0 && option > 4)
+   if(option < 1 && option > 4)
    {
      cout << "You have chosen an invalid menu option.\n";
      cout << "Please choose from one of the following: \n ";
@@ -45,222 +47,245 @@ int bankMenu()
      cout << "Enter your number choice now: ";
      cin >> option;
     }
-       
     return option;
 }
 
-bankAccount inputAccount(bankAccount customers[], int element)
+bankAccount accountInput(int count)
 {
-  cin.ignore();
-  for(int count = 0; count < element; count++)
-  {
-      //Get the customer name
-      cout << "Please enter the name of customer #" << (count + 1) << ": " << endl;
-      getline(cin, customers[count].name);
-      
-      //Get the customer Account ID
-      cout << "Please enter customer #" << (count + 1) << "'s account ID: " << endl;
-      cin >> customers[count].ID;
+   cin.ignore();
+   bankAccount tempCustomer;
    
-      //Get the customer balance
-      cout << "Please enter customer  #" << (count + 1) << "'s account balance: " << endl;
-      cin >> customers[count].balance;
-      
-      while(customers[count].balance < 0)
-      {
-        cout << "A customer's balance cannot be less than 0. \n";
-        cout << "Please enter in a new customer balance that ";
-        cout << "is greater than 0: \n";
-        cin >> customers[count].balance;
-      }
+   cout << "Please enter customer #" << (count + 1) << "'s name: ";
+   getline(cin, tempCustomer.name);
+   cout << "Please enter customer #" << (count + 1) << "'s account number: ";
+   cin >> tempCustomer.ID;
+   cout << "Please enter customer #" << (count + 1) << "'s account balance: ";
+   cin >> tempCustomer.balance;
+   
+   if(tempCustomer.balance < 0)
+   {
+      cout << "The account Balance has to be greater than 0.\n";
+      cout << "Please enter a new account Balance from customer #";
+      cout << (count + 1) << ": ";
+      cin >> tempCustomer.balance;
    }
+   return tempCustomer;
 }
+
+int editMenu(int ID)
+{
+    const int NAME_EDIT = 1,           //For Menu for structure edits
+             ID_EDIT = 2,
+             BALANCE_EDIT = 3,
+             QUIT_PROGRAM = 4;
+   int option; 
+
+   cout << "You have chosen to edit Account #" << ID <<".\n";
+   cout << "Your editing options are: \n";
+   cout << "1:Edit the name on the account\n";
+   cout << "2:Change the account ID\n";
+   cout << "3:Change the balance on the account\n";
+   cout << "4:Go bank to the main menu\n";
+   cout << "Please enter you choice now: ";
+   cin >> option;
+   
+   if(option < NAME_EDIT && option > QUIT_PROGRAM)
+   {
+      cout << "Your answer is invalid.\n";
+      cout << "Your editing options are: \n";
+      cout << "1:Edit the name on the account\n";
+      cout << "2:Change the account ID\n";
+      cout << "3:Change the balance on the account\n";
+      cout << "4:Go Back to Main Menu\n";
+      cout << "Please enter you choice now: ";
+      cin >> option;
+   }
+   
+   return option;
+}
+
 
 
 int main()
 {
    //User Documentation 
-   cout << "Welcome Customer Account Manager.\nIn the program, you";
+   cout << "Welcome to the Customer Account Manager.\nIn this program, you";
    cout << " will be able to enter customer infromation,\nedit already";
-   cout << " already existing information. \nAn print a report of the customer";
-   cout << " accounts. \n";
+   cout << " existing customer information. \nAnd print a report of the customer";
+   cout << " accounts inputed during the program. \n";
    
-   //Declarations
-   int count;                //Loop Counte
-   int numOfAccounts;        //To keep track of the number of account in array
-   bankAccount *customers;   //Array to hold accounts
-   int ID;                   //To hold the customer account ID
-   int option1;              //To be used for user options when making edits
-   int index = 0;            //Used as subscript to search for customer ID
-   int position = -1;        //To be used to keep track of position in array
-   bool exist = false;       //To be used for the results of the search
+   int numOfAccounts = 0;      //Size declarator of dynamic array
+   bankAccount *customer;     //Array to be dynaically Allocated
+   int option = bankMenu();   //For Menu Option
+   int count;                 //Loop Counter               
    
-   //Costants
-   const int CUSTOMER_INPUT = 1,    //For Bank Main Menu
-             ACCOUNT_EDIT = 2,
-             PRINT_REPORT = 3,
-             QUIT_PROGRAM = 4; 
-   const int ID_EDIT = 1,           //For Menu for structure edits
-             NAME_EDIT = 2,
-             BALANCE_EDIT = 3;
-
-   //Dynamically allocating space for customers array 
-   customers = new bankAccount[numOfAccounts];
-  
-  option1 = bankMenu(); 
-  
-  while(option1 != 4)
-  {     
-       switch(option1)
-       {
-         case CUSTOMER_INPUT:
-           cout << "This works";
-          //  //Obtain the number of accounts to be held in *customers array
-//            cout << "How many accounts do you plan to work with today?: ";
-//            cin >> numOfAccounts;
-//    
-//       //   //Input Validation
-// //         while(numOfAccounts < 20)
-// //         {
-// //             cout << "You have to work with at least 20 accounts at a time.\n";
-// //             cout << "Please enter the number of account you plan to work";
-// //             cout << " today.";
-// //             cin >> numOfAccounts;
-// //         } 
-//            
-//            //Input account information in customers array
-//             cout << "Please fill in every field prompted, an incomplete account ";
-//             cout << "will not be accepted. \n";
-//    
-//             for(count = 0; count < numOfAccounts; count++)
-//             {   
-//                customers[count] = inputAccount(customers, count);
-//             }
-            break;
-         case ACCOUNT_EDIT:
-             cout << "You have chosen to edit one of the customer";
-             cout << " accounts.\n You will be able to edit any of the";
-             cout << " customer accounts in the system. \nPlease enter"; 
-             cout << "the customer ID of the customer account you would like to edit:  \n";
-             cin >> ID;
+   //Loop to keep user in program
+   while(option !=4)
+   {
+      switch(option)
+      {
+         case 1:
+         {
+               //User Documentation
+               cout << "You have chosen to input customer accounts.\n";
+               cout << "You will be asked enter the number of accounts\n";
+               cout << " you plan to work with today.";
+               
+               //Obtain the size of the array
+               cout << "Please enter the number of accounts you plan to work with today: ";
+               cin >> numOfAccounts;
    
-             //Seach for account ID
-             while(index < numOfAccounts && !exist)
-             {
-                  if(customers[index].ID == ID)
-                  {
-                     exist = true;      //Mark that ID has been found in array
-                     position = index;   //Mark location of found ID in array
-                  }
-                 index++;
-              }      
-   
-              //Output to user results of search
-              while(exist == false)
-              {
-                  cout << "Sorry, the customer ID you have entered is not in the system. \n";
-                  cout << "Please enter a new customer ID: ";
-                  cin >> ID; 
-               }     
-    
-               while(exist == true)
+             //Input Validation
+             //while(numOfAccounts < 20)
+//             {
+//               cout << "You have to work with at least 20 accounts at a time.\nPlease";
+//               cout << " enter the number of accounts you plan to work with today: ";
+//               cin >> numOfAccounts;
+//              }  
+         
+             //Dynamically Allocate Memory
+               customer = new bankAccount[numOfAccounts];
+               
+               //User Inputs Customer information
+               for(count = 0; count < numOfAccounts; count++)
                {
-                  cout << "You have chosen account number " << ID << "\n";
-                  cout << "Please chose a field that needs to be edited: \n";
-                  cout << "1: Change Account ID \n";
-                  cout << "2: Change Name on Account \n";
-                  cout << "3: Change Current Account Balance \n";
-                  cout << "4: Go Back to Bank Main Menu/n";
-                  cout << "Please enter your choice now:  ";
-                  cin >> option1;
-               
-                  //Input Validation
-                  while(option1 < ID_EDIT && option1 > QUIT_PROGRAM)
-                  {
-                         cout << "You have chosen an invalid number \n";
-                         cout << "Please choose one of the options in the menu.";
-                         cout << "Please chose a field that needs to be edited: \n";
-                         cout << "1: Change Account ID \n";
-                         cout << "2: Change Name on Account \n";
-                         cout << "3: Change Current Account Balance \n";
-                         cout << "4: Go Back to Bank Main Menu/n";
-                         cout << "Please enter your choice now:  ";
-                         cin >> option1;
-                   }
-      
-                  if(option1 == QUIT_PROGRAM)
-                  {
-                     cout << "You have chose to return to the main menu.";
-                     delete [] customers;
-                     exit(0);
-                  }
-      
-                  while(option1 != QUIT_PROGRAM)
-                  {
-                      switch(option1)
-                      {
-                        case ID_EDIT:
-                        {
-                           cout << "You have chosen to edit the account ID";
-                           cout << " for account # " << ID << "\nPlease enter a";
-                           cout << " new ID Number: ";
-                           cin >> customers[position].ID;
-               
-                           cout << "You have successfully changed the customer ID ";
-                           cout << " to " << customers[position].ID << ".\nYou will now";
-                           cout << "now be returned to the menu.";
-                           break;
-                        }  
+                  customer[count] = accountInput(count);
             
-                        case NAME_EDIT:
-                        {
-                           cout << "You have chosen to edit the name on the ";
-                           cout << "account for account #" << ID << "\n Please enter a ";
-                           cout << " new name : ";
-                           getline(cin, customers[position].name);
-                           
-                           cout << "You have successfully changed the name on the ";
-                           cout << "account to:" << customers[position].name << "\n .You ";
-                           cout << "will now be returned to the menu.";
-                           break;
-                        }  
-            
-                        case BALANCE_EDIT:
-                        {
-                           cout << "You have chosen to edit the account balance";
-                           cout << "for the account #" << ID << "\n. Please remember ";
-                           cout << "that only postive balances will be accepted.\n Please" ;
-                           cout << "enter a balance now: ";
-                           cin >> customers[position].balance;
-                           
-                           while(customers[position].balance < 0)
-                           {
-                              cout << "Account Balances cannot be less than $0";
-                              cout << ".\nPlease enter in a new account balance";
-                              cout << " greater than 0: ";
-                              cin >> customers[position].balance;
-                            }
-               
-                            cout << "You have successfully changed the accont balance to: $";
-                            cout << customers[position].balance << ".\n You will not be returned ";
-                            cout << "to the menu.";
-                            break; 
-                          }
-                   }
                }
-           }
-           break;
-         case PRINT_REPORT:
+               
+               //Return User back to Main Menu    
+               option = bankMenu();
+               break;
+          }
+          case 2:
+          {
+              //User Documentation
+              cout << "You have chosen to change information on an existing account.\n";
+              cout << "You will be to input an account ID. If the account exisit, you\n";
+              cout << "will be able to edit the specific account information and then \nwill be";
+              cout << "taken back to the main menu.\n";
+              
+              //Declarations 
+              int ID;                //For user to input account ID
+              int position = -1;     //Mark position in the array when searching
+              bool exist = false;    //Mark the existance of ID after search
+              
+              //Obtain user ID
+              cout << "Please enter the account ID you wish to edit, press 4 to go back to the main menu: ";
+              cin >> ID;
+              
+              //Search of customer array and input validation
+              do{
+                     
+                  int count1 = 0;   //Loop Counter
+                  
+                  //Linear search of customer array for account ID
+                  while(count1 < numOfAccounts && !exist)
+                  {
+                     if(customer[count1].ID == ID)
+                     {
+                     exist = true;
+                     position = count1;
+                     }
+                     count1++;
+                  }
+                  
+                  if(ID == 4)
+                     exist = true;
+                  
+                  //Input Validation
+                  if(!exist)
+                  {
+                     cout << "The account ID you have entered does not exist. \n";
+                     cout << "Plese enter a new ID number or\n enter 4 to go back to the main menu :";
+                     cin >> ID;
+                  }
+                  
+                  
+              }while(!exist);
+              
+              //Give user the option of going back to main menu
+              if(ID == 4)
+              {
+               option = bankMenu();
+               break;
+              }
+                  
+              
+              //Menu and switch statement to edit specific customer account
+              int option1 = editMenu(ID);
+                  
+              switch(option1)
+              {
+                case 1:
+                  cin.ignore();    //Input buffer eraser
+                  
+                  cout << "You have chosen to change the name on accout ID #" << ID << endl;
+                  cout << "Please enter the new name you want to put on account ID #" << ID << ": ";
+                        getline(cin, customer[position].name);
+                        
+                  cout << "You have successfully changed the name on the account to:" << customer[position].name;
+                  cout << ".\nYou will now be taken back to the main menu.\n\n"; 
+                  option = bankMenu();
+                  break;
+                case 2:
+                  cout << "You have chosen to change the account ID for the account ID#" << ID << endl;         
+                  cout << "Please enter the new account ID: ";
+                  cin >> customer[position].ID;
+                  
+                  cout << "You have successfully changed the account ID number to: " << customer[position].ID;
+                  cout << ".\nYou will now be taken back to the main menu.\n\n";
+                  option = bankMenu();
+                  break;
+               case 3:
+                  cout << "You have chosen to change the balance for account ID#" << ID << endl;
+                  cout << "Please enter enter the new balance: ";
+                  cin >> customer[position].balance;
+                  
+                  //Input Validation      
+                  if(customer[position].balance < 0)
+                  {
+                    cout << "The balance of customer's accounts cannot be less than 0\n";
+                    cout << "Please enter in a new balance for account ID#" << ID << ": ";
+                    cin >> customer[position].balance;
+                  }
+                  
+                  cout << "You have successfully changed the account balance to: " << customer[position].balance;
+                  cout << ".\nYou will now be taken back to the main menu.\n\n";
+                  option = bankMenu();
+                  break;
+               case 4:
+                  cout << "You will now be taken back to the main menu. \n\n";
+                  option = bankMenu();
+                  break;
+               }
+              break; 
+             }
+          case 3:
+          {
             cout << "\tCustomer Reports\n";
             cout << "Account ID" << "\t\t\t" << "Customer Name" << "\t\t\t" << "Account Balance" << endl;
-            cout << "-----------" <<"\t\t\t" << "-------------" << "\t\t\t" << "---------------" << endl;  
-            for(count = 0; count < numOfAccounts; count++)
+            cout << "-----------" <<"\t\t\t" << "-------------" << "\t\t\t" << "---------------" << endl; 
+            for(int count2 = 0; count2 < numOfAccounts; count2++)
             {
-               cout << customers[count].ID << "\t\t\t" << customers[count].name << "\t\t\t" << customers[count].balance;
+              cout << customer[count2].name << "\t\t" << customer[count2].ID << "\t\t" << customer[count2].balance << endl;
             }
+            
+            "\n\nYou will now be taken back to the main menu.";
+            option = bankMenu();
             break;
-       } 
+          }
+        }
      }
-    delete []customers;
-    cout << "You have chosen to quit the program.";
+      //Option for User to quit the program
+      if(option == 4)
+      {
+         cout << "You have chosen to quit the program.";
+         if(numOfAccounts != 0)
+            //Dynamically Delete memory in Array
+            delete[] customer;
+         return 0;
+       }
+          
 }
+
